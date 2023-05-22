9ff6896b770ec4e000ed2be1f76fa67f55c1a222
hodgek-py <85642867+hodgek-py@users.noreply.github.com>
2023-05-22 13:31:28 -0600

    New methods

--- Farm-Sim/Farm_Sim.py
+++ Farm-Sim/Farm_Sim.py
@@ -8,20 +8,28 @@ class Farm_Sim:
         self.counter = counter
 
     def simulation(self):
-        sim = Sim_Var(2,1,50,2)
-        print(sim.spread)
+        sim = Sim_Var(2,1,3,2)
+        print(sim.final_output())
         while self.time > self.counter:
             print(self.counter)
             self.counter += 1
 
 class Sim_Var:
-    def __init__(self,crop,rain,size,fertilizer,ratio = (1/3)):
+    def __init__(self,crop,rain,size,fertilizer,ratio = (1/3),output = 0):
         self.crop = crop
         self.rain = rain
         self.size = size
         self.fertilizer = fertilizer
         self.ratio = ratio
-        self.spread = self.ratio * self.size
+        self.seeds = (self.ratio * (self.size * 12)) * (self.ratio * (self.size * 12))
+        self.output = output
+
+    def final_output(self):
+        for x in range(0,int(self.seeds)):
+            self.output += random.randint(20,25)
+
+        self.output = self.output - self.seeds
+        return self.output
 
     def rain_day(self):
         pass
